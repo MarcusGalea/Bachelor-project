da654ce79ec7ed6d55a15f6bc696d2b35a73dbc7
Sandoar <s194323@student.dtu.dk>
2022-04-25 10:34:11 +0200

    Update model2_training.py

--- scripts/neural_networks/model2_training.py
+++ scripts/neural_networks/model2_training.py
@@ -206,12 +206,10 @@ def get_model_instance_segmentation(num_classes):
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
     # now get the number of input features for the mask classifier
-    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    hidden_layer = 64#256
+    #in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    #hidden_layer = 64#256
     # and replace the mask predictor with a new one
-    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
-                                                       hidden_layer,
-                                                       num_classes)
+    #model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,hidden_layer,num_classes)
 
     return model
 
