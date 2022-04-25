61105edf5a05f0fe082c9e226e9e3696d5ff01e5
Sandoar <s194323@student.dtu.dk>
2022-04-25 10:31:08 +0200

    2_7

--- scripts/neural_networks/model2_training.py
+++ scripts/neural_networks/model2_training.py
@@ -198,7 +198,7 @@ import transforms as T
 
 def get_model_instance_segmentation(num_classes):
     # load an instance segmentation model pre-trained on COCO
-    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
     # get number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
@@ -207,7 +207,7 @@ def get_model_instance_segmentation(num_classes):
 
     # now get the number of input features for the mask classifier
     in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    hidden_layer = 256
+    hidden_layer = 64#256
     # and replace the mask predictor with a new one
     model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
                                                        hidden_layer,
@@ -275,7 +275,7 @@ def main():
         # evaluate on the test dataset
         evaluate(model, data_loader_test, device=device)
         
-    PATH = "NN_2_6.pt"
+    PATH = "NN_2_7.pt"
     torch.save(model.state_dict(), PATH)
 
     print("Finished Training")
