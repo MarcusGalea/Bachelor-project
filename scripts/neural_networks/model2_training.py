21823a9994c30ed82f4713aed83a01bd2cd644ef
Sandoar <s194323@student.dtu.dk>
2022-05-01 08:43:59 +0200

    anchor generator

--- scripts/neural_networks/model2_training.py
+++ scripts/neural_networks/model2_training.py
@@ -190,6 +190,45 @@ from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 import transforms as T
 
+#Faster R-CNN
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+# load a pre-trained model for classification and return
+# only the features
+backbone = torchvision.models.mobilenet_v2(pretrained=True).features
+# FasterRCNN needs to know the number of
+# output channels in a backbone. For mobilenet_v2, it's 1280
+# so we need to add it here
+backbone.out_channels = 1280
+
+# let's make the RPN generate 5 x 3 anchors per spatial
+# location, with 5 different sizes and 3 different aspect
+# ratios. We have a Tuple[Tuple[int]] because each feature
+# map could potentially have different sizes and
+# aspect ratios
+anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
+                                   aspect_ratios=((0.5, 1.0, 2.0),))
+
+# let's define what are the feature maps that we will
+# use to perform the region of interest cropping, as well as
+# the size of the crop after rescaling.
+# if your backbone returns a Tensor, featmap_names is expected to
+# be [0]. More generally, the backbone should return an
+# OrderedDict[Tensor], and in featmap_names you can choose which
+# feature maps to use.
+roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],
+                                                output_size=7,
+                                                sampling_ratio=2)
+
+# put the pieces together inside a FasterRCNN model
+model = FasterRCNN(backbone,
+                   num_classes=5,
+                   rpn_anchor_generator=anchor_generator,
+                   box_roi_pool=roi_pooler)
+#mask R-CNN
+"""
 def get_model_instance_segmentation(num_classes):
     # load an instance segmentation model pre-trained on COCO
     #model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
@@ -206,7 +245,7 @@ def get_model_instance_segmentation(num_classes):
     model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,hidden_layer,num_classes)
 
     return model
-
+"""
 def get_transform(train):
     transforms = []
     transforms.append(T.ToTensor())
@@ -243,7 +282,7 @@ def main():
     # get the model using our helper function
     #model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False, num_classes = num_classes)
     #model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=False,num_classes = num_classes)
-    model = get_model_instance_segmentation(num_classes)
+    #model = get_model_instance_segmentation(num_classes)
     
     # move model to the right device
     model.to(device)
@@ -267,7 +306,7 @@ def main():
         # evaluate on the test dataset
         evaluate(model, data_loader_test, device=device)
         
-    PATH = "NN_2_9.pt"
+    PATH = "NN_2_10.pt"
     torch.save(model.state_dict(), PATH)
 
     print("Finished Training")
