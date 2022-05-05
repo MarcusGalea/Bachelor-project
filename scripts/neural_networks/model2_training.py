6e36a02a6f45d1d284d4c0ef5c2271453e91c2fe
Sandoar <s194323@student.dtu.dk>
2022-05-05 09:56:18 +0200

    Update model2_training.py

--- scripts/neural_networks/model2_training.py
+++ scripts/neural_networks/model2_training.py
@@ -249,7 +249,7 @@ def get_model_instance_segmentation(num_classes):
     
     anchor_generator = AnchorGenerator(
                         sizes=(tuple([(16, 64, 256) for _ in range(5)])),
-                        aspect_ratios=(tuple[(0.1, 1.0, 3.0, 6.0) for _ in range(5)]))
+                        aspect_ratios=(tuple([(0.1, 1.0, 3.0, 6.0) for _ in range(5)]))
     model.rpn.anchor_generator = anchor_generator
     model.rpn.head = RPNHead(256,anchor_generator.num_anchors_per_location)
 
