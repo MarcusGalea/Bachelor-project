14f4cba16c26851edf2aeaa76a019f0a8b2f7846
Sandoar <s194323@student.dtu.dk>
2022-05-05 11:50:08 +0200

    Update model2_training.py

--- scripts/neural_networks/model2_training.py
+++ scripts/neural_networks/model2_training.py
@@ -273,7 +273,7 @@ def main():
                                                    gamma=0.1)
 
     # let's train it for 10 epochs
-    num_epochs = 100
+    num_epochs = 10
 
     for epoch in range(num_epochs):
         # train for one epoch, printing every 10 iterations
