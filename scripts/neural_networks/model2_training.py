f00037b712de987330bfdeb8fdb3b4e301f86c31
Sandoar <s194323@student.dtu.dk>
2022-05-05 10:23:11 +0200

    Update model2_training.py

--- scripts/neural_networks/model2_training.py
+++ scripts/neural_networks/model2_training.py
@@ -243,7 +243,7 @@ def get_model_instance_segmentation(num_classes):
 
     # now get the number of input features for the mask classifier
     in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    hidden_layer = 256
+    hidden_layer = 64#256
     # and replace the mask predictor with a new one
     model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,hidden_layer,num_classes)
     
@@ -317,7 +317,7 @@ def main():
         # evaluate on the test dataset
         evaluate(model, data_loader_test, device=device)
         
-    PATH = "NN_3_1.pt"
+    PATH = "NN_3_2.pt"
     torch.save(model.state_dict(), PATH)
 
     print("Finished Training")
